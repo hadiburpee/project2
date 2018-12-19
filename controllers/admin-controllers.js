268ad933245caeec0424aa911f0dab616e6449e6
hadiburpee <hadiburpee@gmail.com>
2018-12-18 21:59:27 -0800

    added a simple post request for admin to create new rule

--- controllers/admin-controllers.js
+++ controllers/admin-controllers.js
@@ -1,10 +1,24 @@
 //routes for admin controllers
-
+var db = require("../models");
 
 exports.index = function(req,res){
     console.log("admin controller");
     res.render('manager/manage');
 
 };
+
+
+exports.addRule = function (req, res) {
+    console.log("Add rule hit");
+
+    db.nexus_rules.create({
+        state_name: req.body.state,
+        transaction: req.body.trans,
+        sales: req.body.sales
+    }).then(function(dbnewClient) {
+        res.json(dbnewClient);
+    });
+};
+
 
 
